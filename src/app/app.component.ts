d9545a77d0f607ee79660331f9634d1f44a45084
Marcin Partyka <marcin.partyka@forcom.com.pl>
2024-10-30 00:12:01 +0100

    Scanner and camera rutes added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
@@ -10,6 +10,8 @@ import {FormsModule} from '@angular/forms';
   standalone: true,
   imports: [
     RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
     MatSlideToggleModule,
     MatInputModule,
     MatFormFieldModule,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import {ScannerComponent} from './scanner/scanner.component';
+import {CameraComponent} from './camera/camera.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path: 'scanner',
+        title: 'Scaanner page title',
+        component: ScannerComponent
+    },
+    {
+        path: 'camera',
+        title: 'Camera page title',
+        component: CameraComponent },
+];
